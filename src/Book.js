c409b7c1b36aff9b674de441d1b6d988a92759f9
Alba Gonzalez <albupas@hotmail.com>
2018-12-19 11:22:16 +0000

    fix: Assign shelf=none when there is no shelf property

--- src/Book.js
+++ src/Book.js
@@ -8,13 +8,16 @@ class Book extends Component {
     let imageHolder = "";
     let hasPropImg = this.props.book.hasOwnProperty('imageLinks');
     if(hasPropImg) {imageHolder = this.props.book.imageLinks.smallThumbnail};
+    let currentShelf = "none";
+    let hasPropShelf = this.props.book.hasOwnProperty('shelf');
+    if(hasPropShelf) {currentShelf = this.props.book.shelf};
     
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: "url('"+imageHolder+"')" }}></div>
           <div className="book-shelf-changer">
-            <select value={this.props.book.shelf} onChange={() => this.props.onMoveBook(this.props.book)}>
+            <select value={currentShelf} onChange={() => this.props.onMoveBook(this.props.book)}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
